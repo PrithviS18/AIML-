f89086e4c4d8bf040d065827de9fb420a3a95fe5
Prithvi Sen <113365172+PrithviS18@users.noreply.github.com>
2024-10-18 17:50:25 +0530

    Update KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -1,14 +1,33 @@
-from sklearn.datasets import load_iris
+import pandas as pd
+data = pd.read_csv('/content/cluster_data (1).csv')
+data.head()
+from sklearn.cluster import KMeans
+model = KMeans(n_clusters = 2)
+model.fit(data)
+print(model.labels_)
+print (model.cluster_centers_)
+from matplotlib import pyplot as plt
+plt.scatter(data.iloc[model.labels_ == 0,0], data.iloc[model.labels_ == 0, 1], c = 'red',label = 'Cluster 0')
+plt.scatter(data.iloc[model.labels_ == 1,0], data.iloc[model.labels_ == 1, 1], c = 'blue', label = 'Cluster 1')
+plt.scatter(model.cluster_centers_[:,0], model.cluster_centers_[:,1], c = 'yellow', label = 'Centroids' )
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend()
+plt.show()
 
-data = load_iris()
-type(data)
-X = data.data #features
-Y = data.target #target
-from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = 0.2) #30% of the data will be used for testing
-from sklearn.neighbors import KNeighborsClassifier
-model = KNeighborsClassifier(n_neighbors=10)
-model.fit(X_train,Y_train)
-Y_pred = model.predict(X_test)
+print(model.inertia_)
+#How to find the optimal number of clusters
+#Elbow Method
 
-print(Y_pred)
+maxx=0
+score=0
+
+for k in range(2,9):
+  model2 = KMeans(n_clusters = k)
+  model2.fit(data)
+  val= model2.inertia_;
+  if(maxx<val):
+    maxx=val
+    score=k
+
+print(k)
